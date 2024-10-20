a59cd0ab69c760deea0264db6c2ee02ee41126b3
yU93r <ugurskahriman@gmail.com>
2024-10-20 23:20:01 +0300

    Update steamclient.cpp

--- steamclient/steamclient.cpp
+++ steamclient/steamclient.cpp
@@ -1,18 +1,21 @@
-
-//#include "dll.h"
 #define WIN32_LEAN_AND_MEAN
+
+// #include "dll.h"
 #include "Windows.h"
+
 #ifdef _WIN64
 #define DLL_NAME "steam_api64.dll"
 #else
 #define DLL_NAME "steam_api.dll"
 #endif
 
-extern "C" __declspec( dllexport )  void *CreateInterface( const char *pName, int *pReturnCode )
+extern "C" __declspec(dllexport) void *CreateInterface(const char *pName, int *pReturnCode)
 {
-    //PRINT_DEBUG("%s", pName);
+    // PRINT_DEBUG("%s", pName);
+
     HMODULE steam_api = LoadLibraryA(DLL_NAME);
-    void *(__stdcall* create_interface)(const char*) = (void * (__stdcall *)(const char*))GetProcAddress(steam_api, "SteamInternal_CreateInterface");
+
+    void *(__stdcall * create_interface)(const char *) = reinterpret_cast<void *(__stdcall *)(const char *)>(GetProcAddress(steam_api, "SteamInternal_CreateInterface"));
 
     return create_interface(pName);
 }
